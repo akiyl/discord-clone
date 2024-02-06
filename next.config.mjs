768b888a03e829d02a817ca754b95afac0777297
akiyl <akshatthapliyal2004@gmail.com>
2024-02-06 12:53:27 +0530

    navbar-fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     images: {
+          domains: [
+               "uploadingthing.com",
+               "utfs.io"
+          ]
+     }
+};
 
 export default nextConfig;
